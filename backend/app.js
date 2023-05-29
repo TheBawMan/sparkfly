39f529aa3751802dc5cef23d3d0249156892db0b
TheBawMan <104532960+TheBawMan@users.noreply.github.com>
2023-05-29 12:55:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://spakrkfly.vercel.app/',
+  origin: 'https://spakrkfly.vercel.app',
   credentials: true
 }));
 
