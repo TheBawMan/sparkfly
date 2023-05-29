e18daf9bfd4ebe112a8aac4d94ab528e75b40af1
TheBawMan <104532960+TheBawMan@users.noreply.github.com>
2023-05-29 12:39:55 +0530

    Update SellerActivationPage.jsx

--- frontend/src/pages/SellerActivationPage.jsx
+++ frontend/src/pages/SellerActivationPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useNavigate } from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 import { server } from "../server";
 
 const SellerActivationPage = () => {
